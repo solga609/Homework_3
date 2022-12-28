0e065f16cfd4343980b6c3af3e39cfd97a7803a2
Olga <olga609@mail.ru>
2022-12-28 19:29:56 +0200

    Third_work

---
+++ home_work_3.java
@@ -0,0 +1,57 @@
+
+// Реализовать алгоритм сортировки слиянием
+import java.util.Arrays;
+
+public class home_work_3 {
+   
+public static void main(String[] args) {
+        int[] array1 = { 6, 8, 9, -6, -11, 1, -9, 2, -179, -2, 32, 7 };
+        int[] result = mergesort(array1);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] mergesort(int[] array1) {
+        int[] buffer1 = Arrays.copyOf(array1, array1.length);
+        int[] buffer2 = new int[array1.length];
+        int[] result = mergesortInner(buffer1, buffer2, 0, array1.length);
+        return result;
+    }
+
+    /**
+     * @param buffer1
+     * @param buffer2
+     * @param startIndex
+     * @param endIndex
+     * @return
+     */
+    public static int[] mergesortInner(int[] buffer1, int[] buffer2,
+            int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergesortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergesortInner(buffer1, buffer2, middle, endIndex);
+
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+}
+
+
+
